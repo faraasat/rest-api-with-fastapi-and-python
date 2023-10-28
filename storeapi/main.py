a40c5a36cf1b00434d0a3bfa5730070a1b557078
Farasat Ali <farasat.ali@starmarketingonline.com>
2023-10-28 23:08:43 +0500

    Added Logging

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+import logging
 
+from storeapi.loggin_conf import configure_logging
 from storeapi.database import database
 from storeapi.routers.post import router as post_router
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
     await database.connect()
+    logger.info("Database Connected")
     yield
     await database.disconnect()
 
